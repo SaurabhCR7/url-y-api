2861854121329eca9a5f23a9b32c6c5b794cb6c8
Saurabh Chauhan <saurabhc006@gmail.com>
2019-08-15 23:26:58 +0530

    Fixed heroku

--- routes/api/shorten.js
+++ routes/api/shorten.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const uniqid = require('uniqid');
 
 const URL = require('./../../models/urls');
 
+router.use(cors());
 // router.use((req, res, next) => {
 // 	res.header('Access-Control-Allow-Origin', '*');
 // 	res.header(
