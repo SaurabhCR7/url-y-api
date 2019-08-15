707f12c76ae420be57efc17b6da6492eb9dfc7fa
Saurabh Chauhan <saurabhc006@gmail.com>
2019-08-15 23:36:09 +0530

    Fixed heroku

--- routes/api/shorten.js
+++ routes/api/shorten.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
+//const cors = require('cors');
 const uniqid = require('uniqid');
 
 const URL = require('./../../models/urls');
 
-router.use(cors());
-// router.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	next();
-// });
+//router.use(cors());
+router.use(function(req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Request methods you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	);
+
+	// Request headers you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type'
+	);
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	// Pass to next layer of middleware
+	next();
+});
 router.get('/test', (req, res) => res.json({ msg: 'API is working fine!' }));
 
 router.post('/', (req, res) => {
